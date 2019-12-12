2d15494bb4c645221e7f16e4c58ccd77b6ab63b6
szc-0815 <shizhichao5@outlook.com>
2019-12-12 09:32:17 +0800

    上传controller层

--- cms_shuju/src/main/java/com/shizhichao/bawei/controller/IndexController.java
+++ cms_shuju/src/main/java/com/shizhichao/bawei/controller/IndexController.java
@@ -2,12 +2,24 @@ package com.shizhichao.bawei.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shizhichao.bawei.service.UserService;
 
 @Controller
-public class ShuController {
+@RequestMapping("/")
+public class IndexController {
 
 	@Autowired
 	private UserService service;
+	
+	
+	@RequestMapping("")
+	public String index() {
+		return "index";
+		
+	}
+	
+	
+	
 }
---
+++ cms_shuju/src/main/java/com/shizhichao/bawei/controller/LoginController.java
@@ -0,0 +1,5 @@
+package com.shizhichao.bawei.controller;
+
+public class LoginController {
+
+}
